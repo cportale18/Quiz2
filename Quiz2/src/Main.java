d761f98d3e2abe67d45427c74bcf2237ac8f34b5
josephportale <josephportale@10.0.0.82>
2016-09-27 01:43:32 -0400

    Quiz 2

---
+++ Quiz2/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+	
+	private int Tuition = 12342;
+	final double Rate = 1.16; {
+
+	
+	for (int i = 0; i < 4; i++) {
+		double newTuition = 0;
+	}
+	
+	double newTuition = Tuition * Rate * Rate * Rate;
+}
+}
+
